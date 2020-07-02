fc13e499826e6cad741cd28f413bf05ee0b5327f
KK <dengwq1997@163.com>
2020-07-02 21:23:17 +0800

    Add files via upload

---
+++ Java_Note/day00/Helloworld.java
@@ -0,0 +1,7 @@
+package com.wekkin.day00;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ Java_Note/day01/ConstantDemo.java
@@ -0,0 +1,26 @@
+//comment是注释的意思
+//用于单行注释
+/*
+用于多行注释
+ */
+package com.wekkin.day01;
+
+public class ConstantDemo {
+    public static void main(String[] args){
+        //输出整数
+        System.out.println(100);
+        //输出小数整数
+        System.out.println(0.100);
+        //输出字符串
+        System.out.println("我要学Java");
+        //输出布尔常量
+        System.out.println(true);
+        System.out.println(false);
+        //输出字符常量
+        System.out.println('J');
+        System.out.println("\n字符常量，里面必须给内容(字符)，而且必须是单个字符！");
+
+        System.out.println("");
+
+    }
+}
